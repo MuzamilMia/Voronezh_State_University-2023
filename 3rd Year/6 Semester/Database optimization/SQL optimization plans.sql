f66a905d594103ca60f5e1e2d76532c1f3db0d1a
Muzamil Mia <103633759+MuzamilMia@users.noreply.github.com>
2026-04-10 13:53:37 +0300

    Update SQL optimization plans.sql

--- 3rd Year/6 Semester/Database optimization/SQL optimization plans.sql
+++ 3rd Year/6 Semester/Database optimization/SQL optimization plans.sql
@@ -1,4 +1,92 @@
-	-- 41. Выбрать курс, который сдало наибольшее количество человек. (we have to check the condition as well)
+--47. Выбрать обучающихся, которые нарушили цепочку зависимостей курсов, т. е. сдали успешно один курс, но не сдали предшествующий им курс.
+EXPLAIN (ANALYZE, BUFFERS, VERBOSE)
+SELECT DISTINCT
+    S.StudentID,
+    S.SecondName,
+    S.FirstName,
+    S.NikeName,
+    C.Name,
+    PC.Name
+FROM Student S
+    JOIN StudentPerformance SP ON S.StudentID = SP.StudentID
+    JOIN Course C ON SP.CourseID = C.CourseID
+    JOIN Course_Dependenc CD ON C.CourseID = CD.CourseID
+    JOIN Course PC ON CD.DependsOnCourseID = PC.CourseID
+WHERE 
+    (SP.Marks >= 3 OR SP.status_id IN (SELECT status_id FROM Status WHERE Name IN ('Completed', 'зачет')))
+    AND NOT EXISTS (
+        SELECT 1
+        FROM StudentPerformance SP2
+        WHERE SP2.StudentID = S.StudentID
+            AND SP2.CourseID = CD.DependsOnCourseID
+            AND (SP2.Marks >= 3 OR SP2.status_id IN (SELECT status_id FROM Status WHERE Name IN ('Completed', 'зачет')))
+    )
+ORDER BY S.secondname, S.FirstName;
+------------------------------------------------------------------------------------
+EXPLAIN (ANALYZE, BUFFERS, VERBOSE)
+WITH SuccessfulCourses AS (
+    SELECT 
+        SP.StudentID,
+        SP.CourseID,
+        SP.Marks,
+        ST.Name AS status_name
+    FROM StudentPerformance SP
+        LEFT JOIN Status ST ON SP.status_id = ST.status_id
+    WHERE SP.Marks >= 3 OR ST.Name IN ('Completed', 'зачет')
+)
+SELECT 
+    S.StudentID,
+    S.Secondname,
+    S.FirstName,
+    S.NikeName,
+    C.Name,
+    PC.Name
+FROM SuccessfulCourses SC
+    JOIN Student S ON SC.StudentID = S.StudentID
+    JOIN Course C ON SC.CourseID = C.CourseID
+    JOIN Course_Dependenc CD ON C.CourseID = CD.CourseID
+    JOIN Course PC ON CD.DependsOnCourseID = PC.CourseID
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM SuccessfulCourses SC2
+    WHERE SC2.StudentID = SC.StudentID
+        AND SC2.CourseID = CD.DependsOnCourseID
+)
+ORDER BY S.secondname, S.FirstName, C.Name;
+
+--==========================================================================
+--44. Выбрать для каждого преподавателя количество курсов, которые он преподает,и общее количество курсов, которые есть в БД.
+EXPLAIN (ANALYZE, BUFFERS, VERBOSE)
+SELECT 
+    T.teacherid,
+    T.lastname,
+    T.firstname,
+    T.middlename,
+    COUNT(DISTINCT CT.courseid) AS Amount_courses_teacher,
+    (SELECT COUNT(*) FROM Course) AS Whole_amount_courses_DataBase
+FROM Teacher T
+    LEFT JOIN Lesson L ON T.teacherid = L.teacherid
+    LEFT JOIN CourseTheme CT ON L.themeid = CT.themeid
+GROUP BY T.teacherid, T.lastname, T.firstname, T.middlename
+ORDER BY Amount_courses_teacher DESC, T.lastname;
+----------------
+EXPLAIN (ANALYZE, BUFFERS, VERBOSE)
+SELECT 
+    T.teacherid,
+    T.lastname,
+    T.firstname,
+    T.middlename,
+    COUNT(DISTINCT CT.courseid) AS Amount_courses_teacher,
+    C.total_courses AS Whole_amount_courses_DataBase
+FROM Teacher T
+LEFT JOIN Lesson L ON T.teacherid = L.teacherid
+LEFT JOIN CourseTheme CT ON L.themeid = CT.themeid
+CROSS JOIN (SELECT COUNT(*) AS total_courses FROM Course) C
+GROUP BY T.teacherid, T.lastname, T.firstname, T.middlename, C.total_courses
+ORDER BY Amount_courses_teacher DESC, T.lastname;
+
+--==========================================================================
+	-- 41. Выбрать курс, который сдало наибольшее количество человек.
 EXPLAIN (ANALYZE, BUFFERS, VERBOSE)
 SELECT 
     C.CourseID,
@@ -9,8 +97,7 @@ JOIN StudentPerformance SP ON C.CourseID = SP.CourseID
 WHERE SP.Marks >= 3
 GROUP BY C.CourseID, C.Name
 ORDER BY Pass_student DESC;
-------
-
+---------------------------------------------------------------
 EXPLAIN (ANALYZE, BUFFERS, VERBOSE)
 WITH CoursePassCounts AS (
     SELECT 
@@ -24,34 +111,6 @@ WITH CoursePassCounts AS (
 )
 SELECT * FROM CoursePassCounts
 	WHERE PassedStudents = (SELECT MAX(PassedStudents) FROM CoursePassCounts);
-	
---========================================================================================================
-EXPLAIN (ANALYZE, BUFFERS, VERBOSE)
-SELECT DISTINCT TE.Name
-FROM Type_equipment TE
-WHERE NOT EXISTS (
-    SELECT 1 FROM Equipment E
-    WHERE E.typeID = TE.typeID AND E.InstallationDate >= CURRENT_DATE - INTERVAL '5 years')
-ORDER BY TE.Name;
---------
 
-EXPLAIN (ANALYZE, BUFFERS, VERBOSE)
-SELECT DISTINCT TE.Name
-FROM Type_equipment TE
-WHERE TE.typeID NOT IN (
-	SELECT E.typeID FROM equipment E
-	WHERE E.InstallationDate >= CURRENT_DATE - INTERVAL '5 years'
-	)
-ORDER BY TE.Name;
---------
 
-EXPLAIN (ANALYZE, BUFFERS, VERBOSE)
-SELECT TE.Name
-FROM Type_equipment TE
-EXCEPT
-SELECT TE2.Name
-FROM Type_equipment TE2
-	JOIN Equipment E ON E.typeID = TE2.typeID
-WHERE E.InstallationDate >= CURRENT_DATE - INTERVAL '5 years'
-ORDER BY Name;
---==========================================================================
+
