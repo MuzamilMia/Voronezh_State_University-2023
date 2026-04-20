62c5266b824e05cd21e1a098f9a43a0e13b4b6c9
Muzamil Mia <103633759+MuzamilMia@users.noreply.github.com>
2026-04-20 11:41:53 +0300

    java

---
+++ 3rd Year/5 Semester/Java/1 Lab/test/test.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+public class test{
+    @test
+    void test_add()
+    {
+        a=23;
+        b=23;
+        if(a==b)
+            print("thery are equal");
+    }
+}
